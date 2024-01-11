f4b9fdec9ff647298611c1c0231d6d25a8893a53
alibek <alibek1024@mail.ru>
2024-01-11 17:00:08 +0600

    add table and other

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './users.scss'
 import {useLocation} from "react-router-dom";
 
 const Users = () => {
 
+    const [loading,setLoading] = useState(false)
+    const [user,setUser] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:8080/users')
+            .then((res)=>res.json())
+            .then((json)=>{
+                setUser(json)
+            })
+            .catch((err)=>{
+                console.log(err)
+            }).finally(()=>setLoading(true))
+    },[])
+
+
+
     return (
         <div>
            <h1>Пользователи</h1>
-            <table >
-                <tr >
-                    <th>имя</th>
-                    <th>email</th>
-                    <th>номер</th>
-                </tr>
-                <tr>
-                    <td>алибек</td>
-                    <td>алибек@mail.ri</td>
-                    <td>055555555</td>
-                </tr>
-            </table>
+
+            {
+                loading === false? <h1>Идет загрузка...</h1>:<table >
+                    <tr >
+                        <th>имя</th>
+                        <th>email</th>
+
+                    </tr>
+
+                        {
+
+                            user.map((el)=>(
+                                <tr>
+
+                                    <td>{el.name}</td>
+                                </tr>
+                            ))
+                        }
+                        {
+
+                            user.map((el)=>(
+                                <tr>
+                                    <td>{el.email}</td>
+
+                                </tr>
+                            ))
+                        }
+
+                </table>
+            }
+
         </div>
     );
 };
