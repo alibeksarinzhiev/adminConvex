eff2e4803723adce40cb89cbb5c0517bbceebe16
invictus1902 <131914647+invictus1902@users.noreply.github.com>
2024-01-11 17:53:14 +0600

    emir 11.01

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -4,57 +4,95 @@ import {useLocation} from "react-router-dom";
 
 const Users = () => {
 
-    const [loading,setLoading] = useState(false)
-    const [user,setUser] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [user, setUser] = useState([])
+    const [reviews, setReviews] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:8080/users')
-            .then((res)=>res.json())
-            .then((json)=>{
+            .then((res) => res.json())
+            .then((json) => {
                 setUser(json)
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err)
-            }).finally(()=>setLoading(true))
-    },[])
+            }).finally(() => setLoading(true))
+    }, [])
 
+    useEffect(() => {
+        fetch('http://localhost:8080/reviews')
+            .then((res) => res.json())
+            .then((json) => {
+                setReviews(json)
+            })
+            .catch((err) => {
+                console.log(err)
+            }).finally(() => setLoading(true))
+    }, [])
 
 
     return (
-        <div>
-           <h1>Пользователи</h1>
+        <section className='users'>
+            <h2>Пользователи</h2>
+            <div className="users__table">
+                {
+                    loading === false ? <h1>Идет загрузка...</h1> :
+                        <table>
+                            <tr>
+                                <th>id</th>
+                                <th>Имя</th>
+                                <th>Фамилия</th>
+                                <th>email</th>
+                            </tr>
 
-            {
-                loading === false? <h1>Идет загрузка...</h1>:<table >
-                    <tr >
-                        <th>имя</th>
-                        <th>email</th>
+                            {
 
-                    </tr>
+                                user.map((el) => (
+                                    <tr>
+                                        <td>{el.id}</td>
+                                        <td>{el.name}</td>
+                                        <td>{el.surname}</td>
+                                        <td>{el.email}</td>
+                                    </tr>
+                                ))
+                            }
 
-                        {
+                        </table>
+                }
+            </div>
 
-                            user.map((el)=>(
-                                <tr>
 
-                                    <td>{el.name}</td>
-                                </tr>
-                            ))
-                        }
-                        {
 
-                            user.map((el)=>(
-                                <tr>
-                                    <td>{el.email}</td>
+            <h2>Пользователи с комментариями и вопросоми</h2>
+            <div className="users__table">
+                {
+                    loading === false ? <h1>Идет загрузка...</h1> :
+                        <table>
+                            <tr>
+                                <th>id</th>
+                                <th>Имя</th>
+                                <th>Номер тел:</th>
+                                <th>email</th>
+                                <th>Комментарии</th>
+                            </tr>
 
-                                </tr>
-                            ))
-                        }
+                            {
 
-                </table>
-            }
+                                reviews.map((el) => (
+                                    <tr>
+                                        <td>{el.id}</td>
+                                        <td>{el.fio}</td>
+                                        <td>{el.number}</td>
+                                        <td>{el.email}</td>
+                                        <td>{el.message}</td>
+                                    </tr>
+                                ))
+                            }
 
-        </div>
+                        </table>
+                }
+            </div>
+        </section>
     );
 };
 
