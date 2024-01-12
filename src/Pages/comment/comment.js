3983ed06c446ae95830f19f7850ce348db4c3a51
Nestashka <kasymovanestana776@gmail.com>
2024-01-12 19:26:20 +0600

    'nesti'

--- src/Pages/comment/comment.js
+++ src/Pages/comment/comment.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './comment.css'
+import img1 from './755743646058281-removebg-preview 1.png'
+import star from './оценка-звез-ы-30705393-removebg-preview 2.png'
 
 const Comment = () => {
     return (
@@ -8,6 +10,17 @@ const Comment = () => {
         <p>Средний балл : </p>
            <h3>4,6</h3>
        </div>
+          <div className="comment__text">
+              <p>Отзывы :</p>
+          </div>
+          <div className="user__one">
+              <img src={img1} alt=""/>
+              <p>Яков Путин</p>
+              <img src={star} alt=""/>
+              <p className="slova">Помог ли вам отзыв </p>
+              <button>Да</button>
+              <button>Нет</button>
+          </div>
       </section>
 );
 };
