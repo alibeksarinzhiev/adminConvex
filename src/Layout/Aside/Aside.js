570220d090c33b5eb0eaf2e0083ced528cc41a58
Nestashka <kasymovanestana776@gmail.com>
2024-01-13 20:36:24 +0600

    'nesti'

--- src/Layout/Aside/Aside.js
+++ src/Layout/Aside/Aside.js
@@ -4,8 +4,7 @@ import logo from '../../Utils/image/Logo (2).png'
 import {Link, useLocation} from "react-router-dom";
 import {CustomContext} from "../../context";
 
-const Aside = () => {
-<<<<<<< HEAD
+const Aside = () =>{
     const [path,setPath]= useState('')
     const location = useLocation()
     console.log(location)
@@ -19,9 +18,8 @@ const Aside = () => {
             }
         },
         [location])
-=======
+
     const {path} = useContext(CustomContext)
->>>>>>> 92344fb0cf15d4e8c5b8f1e5fcf312338b06bf92
     return (
         <aside className='aside'>
             <div className="aside__container">
@@ -29,13 +27,13 @@ const Aside = () => {
                 <h2>Menu</h2>
                 <ul className='aside__menu'>
                     <Link className={`${path ==='главная'?'aside__li active':'aside__li'}`} to='/' >Главная</Link>
-<<<<<<< HEAD
+
                     <Link>Товары</Link>
                     <Link className={`${path ==='отзывы'?'aside__li active':'aside__li'}`} to='/comment'>Отзывы</Link>
-=======
+
                     <Link className={`${path ==='Товары'?'aside__li active':'aside__li'}`} to='/product'>Товары</Link>
                     <Link>Отзывы</Link>
->>>>>>> 92344fb0cf15d4e8c5b8f1e5fcf312338b06bf92
+
                     <Link className={`${path ==='пользователи'?'aside__li active':'aside__li'}`} to='/users'>Пользователи</Link>
                 </ul>
             </div>
